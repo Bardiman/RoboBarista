36bec4d71a2a771364b2f99ce34d6da8d1f9d76a
Bardiman <168379129+Bardiman@users.noreply.github.com>
2025-03-29 15:13:18 +0300

    Add files via upload

---
+++ RoboBarista.cpp
@@ -0,0 +1,69 @@
+﻿#include <vector>
+
+class pixel {
+    int r;
+    int g;
+    int b;
+};
+
+class image {
+    pixel pic[1280][1024];  
+    image GetImage() {
+        image pic;
+        return pic;
+    }
+};
+
+class Camera {
+public:
+    Camera() {
+    }
+    image captureImage() {
+        image pic;
+        return pic;
+    }
+};
+
+class PathPlanner {
+public:
+    std::vector<int> calculatePath(const image& pic) {
+        std::vector<int> path;
+        return path;
+    }
+};
+
+class MotorController {
+public:
+    void moveForward(int speed) {
+    }
+
+    void moveBackward(int speed) {
+    }
+
+    void turnLeft(int angle) {
+    }
+
+    void turnRight(int angle) {
+    }
+
+    void stop() {   
+    }
+
+    void move(const std::vector<int>& path) {
+    }
+};
+
+class Robot {
+private:
+    Camera camera;
+    PathPlanner pathPlanner;
+    MotorController motorController;
+
+public:
+    void moveFromAToB() {
+        image pic = camera.captureImage();
+
+        std::vector<int> path = pathPlanner.calculatePath(pic);
+        motorController.move(path);
+    }
+};
